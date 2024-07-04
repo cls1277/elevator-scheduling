4cf28a2b7018b2577a94008df057e165afe7cf19
cls1277 <cls1277@163.com>
2024-07-05 00:37:36 +0800

    增加两个测试实例

--- homework7/ElevatorInput.cs
+++ homework7/ElevatorInput.cs
@@ -6,17 +6,30 @@ class ElevatorInput {
         requests = new ConcurrentQueue<PersonRequest>();
 
         // PersonRequest request = new(2, 6, 400, 1, 5, 2.1);
-        // requests.Add(request);
+        // requests.Enqueue(request);
         // request = new(1, 1, 10, 2, -1, 5.6);
-        // requests.Add(request);
+        // requests.Enqueue(request);
 
-        PersonRequest request = new(2, 8, 300, 1, 5, 1.4);
+        // PersonRequest request = new(2, 8, 300, 1, 5, 1.4);
+        // requests.Enqueue(request);
+        // request = new(1, 2, 9, 1, -1, 4.1);
+        // requests.Enqueue(request);
+        // request = new(2, 8, 300, 2, 6, 5.3);
+        // requests.Enqueue(request);
+        // request = new(2, 6, 400, 6, 8, 8.9);
+        // requests.Enqueue(request);
+
+        PersonRequest request = new(1, 10, 6, 2, -1, 1.9);
         requests.Enqueue(request);
-        request = new(1, 2, 9, 1, -1, 4.1);
+        request = new(1, 11, 6, 3, -1, 5.6);
         requests.Enqueue(request);
-        request = new(2, 8, 300, 2, 6, 5.3);
+        request = new(0, 6, 300, 1, -1, 8.5);
         requests.Enqueue(request);
-        request = new(2, 6, 400, 6, 8, 8.9);
+        request = new(1, 3, 5, 1, -1, 14.6);
+        requests.Enqueue(request);
+        request = new(2, 6, 400, 1, 5, 15.5);
+        requests.Enqueue(request);
+        request = new(1, 1, 11, 9, -1, 16.6);
         requests.Enqueue(request);
     }
      public PersonRequest? nextPersonRequest() {
